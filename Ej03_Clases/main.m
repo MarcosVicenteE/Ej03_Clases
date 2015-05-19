c72e3033478e41e52daa99bd07664aff02e086c3
Marcos Vicente Espasandín <tarde@iMac-de-Profesor-3.local>
2015-05-19 19:42:15 +0200

    Initial Commit

---
+++ Ej03_Clases/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ej03_Clases
+//
+//  Created by tarde on 19/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

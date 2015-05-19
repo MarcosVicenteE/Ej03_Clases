c8554d4d9fb7f5bc862024ad4608bd2a88c2f613
Marcos Vicente Espasandín <tarde@iMac-de-Profesor-3.local>
2015-05-19 20:06:41 +0200

    Initial Commit

---
+++ Ej03_Clases/Modelos/Cliente.h
@@ -0,0 +1,38 @@
+//
+//  Cliente.h
+//  Ej03_Clases
+//
+//  Created by tarde on 19/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cliente : NSObject
+
+//IVARS (Atributos)
+
+{
+    //por defecto @protected, excepto que le indiquemos lo contrario.
+@public
+    //a partir de aqui, se consideran todas publicas
+    int _codigo;
+    long _telefono;
+    char _letra;
+    BOOL _activo;
+}
++(void)detectarMetodo;
+-(id) initWithCodigo:(int)codigo
+             telefono:(long)telefono
+               letra:(char)letra
+              activo:(BOOL)activo
+             nomebre:(NSString*) nombre
+           direccion:(NSString*)direccion;
+-(void) saltar;
+-(NSString*) getDatos;
+-(void)saltarAltura:(int)altura;
+-(void)correrDistancia:(int)distancia
+            aVelocidad:(int)velocidad;
+
+
+@end
---
+++ Ej03_Clases/Modelos/Cliente.m
@@ -0,0 +1,13 @@
+//
+//  Cliente.m
+//  Ej03_Clases
+//
+//  Created by tarde on 19/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import "Cliente.h"
+
+@implementation Cliente
+
+@end
